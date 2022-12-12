dcd0e344488cf67a60dd6258ebcdf034e0b0f043
Needlide <max.litvinyuck553@gmail.com>
2022-12-12 15:55:18 +0200

    Added EquationAnalyzer class. GUI done. Buttons with digits, CE, C, 1/x, Backspace done. (#1)

---
+++ EquationAnalyzer.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal enum Action
+    {
+        None,
+        Plus,
+        Minus,
+        Multiplication,
+        Division,
+        Percentage,
+        Power,
+        Root
+    }
+
+    internal class EquationAnalyzer
+    {
+        decimal _firstNumber;
+        decimal _secondNumber;
+        Action _action = Action.None;
+        string _errorMessage = string.Empty;
+
+        internal decimal FirstNumber { get => _firstNumber; set => _firstNumber = value; }
+        internal decimal SecondNumber { get => _secondNumber; set => _secondNumber = value; }
+        internal Action Action { get => _action; set => _action = value; }
+        internal string ErrorMessage { get => _errorMessage; }
+        
+        internal decimal Calculate()
+        {
+            switch(_action)
+            {
+                case Action.Plus:
+                    return Add();
+                case Action.Minus:
+                    return Subtract();
+                case Action.Multiplication:
+                    return Multiply();
+                case Action.Division:
+                    return Divide();
+                case Action.Percentage:
+                    return Percentage();
+                case Action.Power:
+                    return Power();
+                case Action.Root:
+                    return Root();
+                default:
+                    return decimal.Zero;
+            }
+        }
+
+        decimal Add()
+        {
+            decimal result = decimal.Zero;
+            try
+            {
+                result = decimal.Add(_firstNumber, _secondNumber);
+            }
+            catch (OverflowException)
+            {
+                _errorMessage = "OverflowException";
+            }
+            return result;
+        }
+
+        decimal Subtract()
+        {
+            decimal result = decimal.Zero;
+            try
+            {
+                result = decimal.Subtract(_firstNumber, _secondNumber);
+            }
+            catch (OverflowException)
+            {
+                _errorMessage = "OverflowException";
+            }
+            return result;
+        }
+
+        decimal Multiply()
+        {
+            decimal result = decimal.Zero;
+            try
+            {
+                result = decimal.Multiply(_firstNumber, _secondNumber);
+            }
+            catch (OverflowException)
+            {
+                _errorMessage = "OverflowException";
+            }
+            return result;
+        }
+
+        decimal Divide()
+        {
+            decimal result = decimal.Zero;
+            try
+            {
+                result = decimal.Divide(_firstNumber, _secondNumber);
+            }
+            catch (OverflowException)
+            {
+                _errorMessage = "OverflowException";
+            }
+            catch(DivideByZeroException)
+            {
+                _errorMessage = "Cannot divide by zero";
+            }
+            return result;
+        }
+
+        decimal Percentage()
+        {
+            if(_secondNumber == decimal.Zero)
+            {
+                return (_firstNumber / 100m);
+            }
+            else
+            {
+                return ((_firstNumber / 100m) * _secondNumber);
+            }
+        }
+
+        decimal Power()
+        {
+            decimal result = _firstNumber;
+
+            for(int i = 0; i < _secondNumber; i++)
+            {
+                result *= _firstNumber;
+            }
+
+            return result;
+        }
+
+        decimal Root()
+        {
+            if (_firstNumber < 0m) throw new OverflowException("Cannot calculate square root from a negative number");
+            return RecursionRoot(_firstNumber, null);
+        }
+
+        decimal RecursionRoot(decimal x, decimal? guess = null)
+        {
+            var ourGuess = guess.GetValueOrDefault(x / 2m);
+            var result = x / ourGuess;
+            var average = (ourGuess + result) / 2m;
+
+            if (average == ourGuess)
+                return average;
+            else
+                return RecursionRoot(x, average);
+        }
+    }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 namespace Calculator
 {
@@ -20,9 +10,124 @@ namespace Calculator
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        EquationAnalyzer _equationAnalyzer = new EquationAnalyzer();
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+        }
+
+        private void page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.NewSize.Height <= 800)
+            {
+                foreach (Button button in PanelWithButtons.Children.Cast<Button>())
+                {
+                    button.FontSize = 20;
+                }
+            }
+            else if (e.NewSize.Height > 800)
+            {
+                foreach (Button button in PanelWithButtons.Children.Cast<Button>())
+                {
+                    button.FontSize = 28;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                switch (button.Content.ToString())
+                {
+                    case "0":
+                        if(!EquationBlock.Text.StartsWith("0") && string.IsNullOrEmpty(EquationBlock.Text)) { EquationBlock.Text = "0"; }
+                        else if(!string.IsNullOrEmpty(EquationBlock.Text) && !EquationBlock.Text.Equals("0")) { EquationBlock.Text += "0";}
+                        break;
+                    case "1":
+                        if (EquationBlock.Text.Equals("0")) { EquationBlock.Text = "1"; }
+                        else { EquationBlock.Text += "1"; }
+                        break;
+                    case "2":
+                        if (EquationBlock.Text.Equals("0")) { EquationBlock.Text = "2"; }
+                        else { EquationBlock.Text += "2"; }
+                        break;
+                    case "3":
+                        if (EquationBlock.Text.Equals("0")) { EquationBlock.Text = "3"; }
+                        else { EquationBlock.Text += "3"; }
+                        break;
+                    case "4":
+                        if (EquationBlock.Text.Equals("0")) { EquationBlock.Text = "4"; }
+                        else { EquationBlock.Text += "4"; }
+                        break;
+                    case "5":
+                        if (EquationBlock.Text.Equals("0")) { EquationBlock.Text = "5"; }
+                        else { EquationBlock.Text += "5"; }
+                        break;
+                    case "6":
+                        if (EquationBlock.Text.Equals("0")) { EquationBlock.Text = "6"; }
+                        else { EquationBlock.Text += "6"; }
+                        break;
+                    case "7":
+                        if (EquationBlock.Text.Equals("0")) { EquationBlock.Text = "7"; }
+                        else { EquationBlock.Text += "7"; }
+                        break;
+                    case "8":
+                        if (EquationBlock.Text.Equals("0")) { EquationBlock.Text = "8"; }
+                        else { EquationBlock.Text += "8"; }
+                        break;
+                    case "9":
+                        if (EquationBlock.Text.Equals("0")) { EquationBlock.Text = "9"; }
+                        else { EquationBlock.Text += "9"; }
+                        break;
+                }
+            }
+        }
+
+        private void BackspaceButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(EquationBlock.Text.Length != 0)
+            {
+                EquationBlock.Text = EquationBlock.Text.Substring(0, EquationBlock.Text.Length - 1);
+            }
+        }
+
+        private void ClearEntry_Click(object sender, RoutedEventArgs e)
+        {
+            EquationBlock.Text = string.Empty;
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            EquationBlock.Text = string.Empty;
+            HelperBlock.Text = string.Empty;
+        }
+
+        private void Dot_Click(object sender, RoutedEventArgs e)
+        {
+            if(EquationBlock.Text.Length == 0)
+            {
+                EquationBlock.Text = "0.";
+            }
+            else if(!EquationBlock.Text.Contains(".")) { EquationBlock.Text += '.'; }
+        }
+
+        private void OneDivideX_Click(object sender, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrEmpty(EquationBlock.Text))
+            try
+            {
+                HelperBlock.Text = $"1/{EquationBlock.Text}";
+                EquationBlock.Text = (1 / Convert.ToDecimal(EquationBlock.Text)).ToString();
+            }
+            catch (FormatException)
+            {
+                EquationBlock.Text = "FormatException";
+            }
+            catch (OverflowException)
+            {
+                EquationBlock.Text = "OverflowException";
+            }
         }
     }
 }
